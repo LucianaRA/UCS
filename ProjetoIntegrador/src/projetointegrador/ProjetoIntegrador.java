27bc8e88f0b39c3110d3c88976eac55725d2d122
Luciana Araujo <72928880+yaagura@users.noreply.github.com>
2021-03-12 15:58:12 -0300

    Add files via upload

---
+++ ProjetoIntegrador/src/projetointegrador/ProjetoIntegrador.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetointegrador;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author Luciana
+ */
+public class ProjetoIntegrador extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane p = new Pane();
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
